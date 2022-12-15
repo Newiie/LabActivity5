facae079e7022c86d6955c5952ea52cef3d74659
Jethro Cenas <jethrocenas@gmail.com>
2022-12-15 20:07:27 +0800

    completed FoodOrder

--- src/FoodOrdering/FoodOrderGUI.java
+++ src/FoodOrdering/FoodOrderGUI.java
@@ -1,6 +1,10 @@
 package FoodOrdering;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
 
 public class FoodOrderGUI extends JFrame{
     private JPanel JPanel1;
@@ -17,6 +21,9 @@ public class FoodOrderGUI extends JFrame{
     private JRadioButton rb15;
     private JButton btnOrder;
 
+    private List<JCheckBox> menu;
+
+    private List<JRadioButton> discounts;
     public static void main(String[] args) {
         FoodOrderGUI frame = new FoodOrderGUI();
         frame.setContentPane(frame.pnlMain);
@@ -26,6 +33,82 @@ public class FoodOrderGUI extends JFrame{
     }
 
     public FoodOrderGUI() {
+        menu = new ArrayList<>();
+        discounts = new ArrayList<>();
+        menu.add(cFries);
+        menu.add(cTea);
+        menu.add(cSundae);
+        menu.add(cSoftDrinks);
+        menu.add(cPizza);
+        menu.add(cBurger);
 
+        discounts.add(rbNone);
+        discounts.add(rb5);
+        discounts.add(rb10);
+        discounts.add(rb15);
+
+        btnOrder.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(null, String.format("Price is = %.02f", total_price()), "Message", 1);
+            }
+        });
+    }
+
+    public double total_price() {
+        double price = 0;
+        for (JCheckBox cb : menu) {
+            if (cb.isSelected()) {
+                String food = cb.getText();
+                switch(food) {
+                    case "Fries":
+                        price += 65;
+                        break;
+
+                    case "Pizza":
+                        price += 100;
+                        break;
+
+                    case "Burger":
+                        price += 80;
+                        break;
+
+                    case "Soft Drinks":
+                        price += 55;
+                        break;
+
+                    case "Tea":
+                        price += 50;
+                        break;
+
+                    case "Sundae":
+                        price += 40;
+                        break;
+                }
+            }
+        }
+
+        for (JRadioButton rb : discounts) {
+            if (rb.isSelected()) {
+                String discount = rb.getText();
+                switch (discount) {
+                    case "None":
+                        break;
+
+                    case "5% Off":
+                        price -= price * 0.05;
+                        break;
+
+                    case "10% Off":
+                        price -= price * 0.10;
+                        break;
+
+                    case "15% Off":
+                        price -= price * 0.15;
+                        break;
+                }
+            }
+        }
+        return price;
     }
 }
