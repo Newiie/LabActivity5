a6cfd2fdef6a30108cf83d28b73618bd711aaf61
Jethro Cenas <jethrocenas@gmail.com>
2022-12-15 16:08:38 +0800

    Renamed JPanel to pnlMain and added SimpleCalculator Function

--- src/SimpleCalc/SimpleCalcGUI.java
+++ src/SimpleCalc/SimpleCalcGUI.java
@@ -9,4 +9,17 @@ public class SimpleCalcGUI extends JFrame {
     private JTextField tfNumber2;
     private JTextField lblResult;
     private JButton btnCompute;
+    private JPanel pnlMain;
+
+    public static void main(String[] args) {
+        SimpleCalcGUI frame = new SimpleCalcGUI();
+        frame.setContentPane(frame.pnlMain);
+        frame.setSize(600, 400);
+        frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+
+    public SimpleCalcGUI() {
+
+    }
 }
